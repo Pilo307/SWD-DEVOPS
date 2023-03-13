0e111ab94bf6d5bacf2a658f79705341919a9e47
Robin <robinbrink40@gmail.com>
2023-03-13 22:16:48 +0100

    dotnet restore

---
+++ Lecture10/Lecture10/Lecture10_1.test/UnitTest1.cs
@@ -0,0 +1,29 @@
+namespace Lecture10_1.test
+{
+    public class Tests
+    {
+        private Flashlight myFlashlight = new Flashlight();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            string expectedResultString = "Turn on lamp\r\n";
+
+            StringWriter stringWriter = new StringWriter();
+
+            Console.SetOut(stringWriter);
+
+            myFlashlight.PowerPressed();
+
+            string consoleOutput = stringWriter.ToString();
+
+            Assert.That(consoleOutput, Is.EqualTo(expectedResultString));
+        }
+    }
+}
